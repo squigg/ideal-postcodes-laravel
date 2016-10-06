f0ff7d8cdeace9c0274b3e933331e1956b5f9a42
Steve Strugnell <github@squigg.co.uk>
2016-10-06 18:58:01 +0100

    Add alias to class name

--- src/IdealPostcodesServiceProvider.php
+++ src/IdealPostcodesServiceProvider.php
@@ -14,7 +14,7 @@ class IdealPostcodesServiceProvider extends ServiceProvider
     {
         $this->publishes([
             __DIR__.'/config/ideal-postcodes.php' => config_path('ideal-postcodes.php'),
-        ]);
+        ], 'config');
     }
 
     /**
@@ -38,18 +38,26 @@ class IdealPostcodesServiceProvider extends ServiceProvider
                         'limit'   => config('ideal-postcodes.limit', 25),
                         ],
                 ];
+
+                if ($fields = $this->getFieldList()) {
+                    $config['query']['fields'] = $fields;
+                }
+
                 return new \GuzzleHttp\Client($config);
             }
         );
 
         // Bind the main class into the IOC
-        $this->app->bind(
+        $this->app->singleton(
             'ideal-postcodes',
             function ($app) {
                 $config = config('ideal-postcodes');
                 return new IdealPostcodes($app['ideal-postcodes-client'], $config);
             }
         );
+
+        // Add an alias so that using the full classname as a dependency will grab this version
+        $this->app->alias(IdealPostcodes::class,'ideal-postcodes');
 
     }
 
@@ -59,6 +67,26 @@ class IdealPostcodesServiceProvider extends ServiceProvider
     public function provides()
     {
         return ['ideal-postcodes-client', 'ideal-postcodes'];
+    }
+
+    /**
+     * Return a combined array of field names, including those to be transformed
+     * @return array
+     */
+    protected function getFieldList()
+    {
+        $fields = config('ideal-postcodes.fields', null);
+
+        if ($fields) {
+            $fields = array_map(function($value) {
+                if (is_array($value)) {
+                    return array_keys($value)[0];
+                }
+                return $value;
+            }, $fields);
+        }
+
+        return $fields;
     }
 
 }
